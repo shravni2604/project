84be08ffc1886fa79183e7dafdf022151e264695
shravni2604 <shravnithorat0504@gmail.com>
2025-01-08 17:46:50 +0530

    commit

---
+++ project.py
@@ -0,0 +1,12 @@
+# Simple Python Program
+
+# Print a message
+print("Hello, World!")
+
+# Perform a basic addition
+num1 = 5
+num2 = 3
+result = num1 + num2
+
+# Print the result
+print("The sum of", num1, "and", num2, "is", result)
